4a204ca6614bcf305c68d330b83f526a72668d9f
August Bresnaider <gusbres@gmail.com>
2023-07-18 00:01:07 -0400

    Oh, it's buggy, but we're getting there (yes I know my file directory is stored)

--- atticus/Source.cpp
+++ atticus/Source.cpp
@@ -1,8 +1,20 @@
 #include <fstream>
+#include <vector>
 
 #include "Book.h"
 
 int main()
 {
-	std::fstream file{}
+	std::fstream file{"C:\\Users\\gusbr\\source\\repos\\atticus\\books.txt", std::ios::in | std::ios::out};
+	std::vector<Book> shelf;
+	while (file.good())
+	{
+		Book b;
+		file >> b;
+		shelf.push_back(b);
+	}
+	for (auto book : shelf)
+	{
+		std::cout << book;
+	}
 }
