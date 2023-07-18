b3baa57f2eb6d74e4aac68c6090e597e74cf3108
August Bresnaider <gusbres@gmail.com>
2023-07-18 14:08:05 -0400

    Added toUpper and toLower functions

--- atticus/utils.h
+++ atticus/utils.h
@@ -6,13 +6,47 @@ namespace utils
 {
 	std::string filename{ "C:\\Users\\gusbr\\source\\repos\\atticus\\books.txt" };
 
+	std::string& toLower(std::string str)
+	{
+		std::string res = "";
+		for (auto c : str)
+		{
+			if (c >= 'A' && c <= 'Z')
+			{
+				res += c + 32;
+			}
+			else
+			{
+				res += c;
+			}
+		}
+		return res;
+	}
+
+	std::string& toUpper(std::string str)
+	{
+		std::string res = "";
+		for (auto c : str)
+		{
+			if (c >= 'a' && c <= 'z')
+			{
+				res += c - 32;
+			}
+			else
+			{
+				res += c;
+			}
+		}
+		return res;
+	}
+
 	void wipeFile(std::fstream& f)
 	{
 		f.open(filename, std::ios::trunc);
 		f.close();
 	}
 
-	void updateFile(std::fstream& f, const std::vector<Book>& shelf)
+	void updateFile(std::fstream& f, std::vector<Book>& shelf)
 	{
 		wipeFile(f);
 		std::sort(shelf.begin(), shelf.end());
