edc967917baa27b675ee1c84564bdee9fc6d35a6
August Bresnaider <gusbres@gmail.com>
2023-07-18 00:01:07 -0400

    Oh, it's buggy, but we're getting there (Hopefully my file directory is no longer stored)

--- atticus/Book.cpp
+++ atticus/Book.cpp
@@ -1,12 +1,12 @@
 #include "Book.h"
 
-#define PLACEHOLDER 219
+#define PLACEHOLDER 64
 
 std::ostream& operator<< (std::ostream& out, const Book& b)
 {
 	const int TABS_AFTER_TITLE = 8;
-	out << b.getTitle();
-	for (int i = 0; i < b.getTitle().size() / 8 - TABS_AFTER_TITLE; i++)
+	out << "test";
+	for (int i = 0; i <  TABS_AFTER_TITLE - b.getTitle().size() / 8; i++)
 	{
 		out << '\t';
 	}
@@ -15,13 +15,13 @@ std::ostream& operator<< (std::ostream& out, const Book& b)
 	int percentCompleteInt = (int)(percentComplete * 100);
 	for (int i = 0; i < percentCompleteInt / 4; i++)
 	{
-		out << 219;
+		out << (char)219;
 	}
-	for (int i = 0; i < 25 - percentCompleteInt; i++)
+	for (int i = 0; i < 25 - percentCompleteInt / 4; i++)
 	{
 		out << '-';
 	}
-	out << "]\t" << b.getPagesRead() << " / " << b.getTotalPages() << '(' << percentCompleteInt << "%)";
+	out << "]\t" << b.getPagesRead() << " / " << b.getTotalPages() << " (" << percentCompleteInt << "%)";
 	out << '\n';
 	return out;
 }
--- atticus/Source.cpp
+++ atticus/Source.cpp
@@ -1,8 +1,20 @@
 #include <fstream>
+#include <vector>
 
 #include "Book.h"
 
 int main()
 {
-	std::fstream file{}
+	std::fstream file{"REDACTED", std::ios::in | std::ios::out};
+	std::vector<Book> shelf;
+	while (file.good())
+	{
+		Book b;
+		file >> b;
+		shelf.push_back(b);
+	}
+	for (auto book : shelf)
+	{
+		std::cout << book;
+	}
 }
